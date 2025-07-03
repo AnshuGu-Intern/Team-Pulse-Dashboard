4be7b9865efcad024a034a63194467f2bdc4d38f
Anshu <177839555+AnshuGu-Intern@users.noreply.github.com>
2025-07-03 14:15:17 +0530

    fix: metadata

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,12 @@ import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
 import "./index.css";
 import { store, persistor } from "./store/store";
-import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <HelmetProvider>
-          <App />
-        </HelmetProvider>
+        <App />
       </PersistGate>
     </Provider>
   </React.StrictMode>
